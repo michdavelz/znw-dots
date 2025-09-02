e2aed4cd5299dbb822644db95612316c36555015
Michael Elzinga <65480321+michdavelz@users.noreply.github.com>
2025-09-02 04:18:37 +0000

    Update b2convert.sh
    added quotes around variables for best practice

--- scripts/b2convert.sh
+++ scripts/b2convert.sh
@@ -4,7 +4,7 @@
 # Accepts as many true/t/yes/y/1 and false/f/no/n/0 inputs as needed.
 
 b2convert() {
-    act=$(sed -e 's/\(true\|t\|yes\|y\)/1/Ig' -e 's/\(false\|f\|no\|n\)/0/Ig' -e 's/[[:blank:]]//g' <<< ${@})
+    act=$(sed -e 's/\(true\|t\|yes\|y\)/1/Ig' -e 's/\(false\|f\|no\|n\)/0/Ig' -e 's/[[:blank:]]//g' <<< "${@}")
     echo $(( 2#$act ))
 }
-b2convert ${@}
+b2convert "${@}"
