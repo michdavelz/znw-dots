3c5cfce3131b6d8e73157e74068a909f35f753b5
Michael Elzinga <65480321+michdavelz@users.noreply.github.com>
2025-09-02 04:27:14 +0000

    Update wppr.sh
    Changed wallpaper dir listing to the find command instead of ls and separated variable declaration and export

--- scripts/wppr.sh
+++ scripts/wppr.sh
@@ -4,6 +4,8 @@
 # and UI, but pywal for accents and such. More to be
 # added to this script down the road
 
-export WALLPAPER_PATH=~/Pictures/Wallpapers/$(ls -1 ~/Pictures/Wallpapers|shuf -n 1)
-swww img --transition-fps 60 $WALLPAPER_PATH || echo "Error setting wallpaper code:$?"
-wal -e -t -s --cols16 -i $WALLPAPER_PATH || echo "Error generating colorscheme code:$?"
+#!/usr/bin/env bash
+WALLPAPER_PATH=$(find "${HOME}"/Pictures/Wallpapers/*|shuf -n 1)
+export WALLPAPER_PATH
+swww img --transition-fps 60 "$WALLPAPER_PATH" || echo "Error setting wallpaper code:$?"
+wal -e -t -s --cols16 -i "$WALLPAPER_PATH" || echo "Error generating colorscheme code:$?"
