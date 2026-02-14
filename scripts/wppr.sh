1b11df86a0e9b5e100d6a69e01cb30f6a1820dd7
Michael Elzinga <65480321+michdavelz@users.noreply.github.com>
2026-02-14 12:07:56 -0700

    Update wppr.sh
    swapped wal and awww img command + fixed typo for wallpaper path

--- scripts/wppr.sh
+++ scripts/wppr.sh
@@ -4,7 +4,7 @@
 # and UI, but pywal for accents and such. More to be
 # added to this script down the road
 WPPR_LOG="${XDG_CACHE_HOME:-$HOME/.cache}/wppr.log"
-WALLPAPER_PATH=$(find "${HOME}"../.me_local/Pictures/Wallpapers/*|shuf -n 1)
+WALLPAPER_PATH=$(find "${HOME}"/../.me_local/Pictures/Wallpapers/*|shuf -n 1)
 export WALLPAPER_PATH && echo "Selected image: $WALLPAPER_PATH" >> "$WPPR_LOG"
 i=0
 until pidof awww-daemon >> /dev/null || [ $i == 5 ]; do
@@ -13,7 +13,7 @@ until pidof awww-daemon >> /dev/null || [ $i == 5 ]; do
    ((i++))
    [ $i == 5 ] && (echo "No awww-daemon process found" &>1 ; exit 1)
 done
-awww img --transition-fps 50 "$WALLPAPER_PATH" >> "$WPPR_LOG"|| echo "Error setting wallpaper - code:$?" &>> "$WPPR_LOG"
 wal -n -s -t -e --cols16 -i "$WALLPAPER_PATH" >> "$WPPR_LOG"|| echo "Error generating colorscheme - code:$?" &>> "$WPPR_LOG"
+awww img --transition-fps 50 "$WALLPAPER_PATH" >> "$WPPR_LOG"|| echo "Error setting wallpaper - code:$?" &>> "$WPPR_LOG"
 kitten themes --reload-in=all pywal >> "$WPPR_LOG"
 kitten @ --to unix:/tmp/kp set-colors ~/.config/kitty/themes/pywal.conf >> "$WPPR_LOG"
